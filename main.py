35145053f04e21fce9f132dbc3e388955b1bb302
Jão <joao_bernardi@outlook.com>
2022-08-13 06:32:00 -0300

    `Main`  - Add main file and requirements.

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+import stackhttp as stack
+from lib import utils
+import pyding
+
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    utils.load_routes()
+    http = stack.HTTP("127.0.0.1", 8000)
+    http.spin_up()
