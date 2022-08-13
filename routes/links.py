b8d638ec9ad02ef2ccc9d076ec6c41b4b3b5f040
Jão <joao_bernardi@outlook.com>
2022-08-13 08:39:08 -0300

    `Routes` - Implement links

--- routes/links.py
+++ routes/links.py
@@ -1,3 +1,6 @@
+from genericpath import isfile
+import os
+import re
 from pyding import on, EventCall
 from stackhttp import Request, Response, HTTP
 import logging
@@ -6,5 +9,19 @@ logger = logging.getLogger(__name__)
 
 @on("http_request", host="links.associacaocet.site")
 def links(event: EventCall, host, path, request, response: Response, server):
-    response.define_code(201, "No Content")
-    event.stop()
+    filename = os.path.basename(path)
+
+    path = re.sub("[.]{2}|[~%]", "", path)
+    path = os.path.join("assets/links/", path)
+
+    while os.path.exists(path):
+        if os.path.isdir(path):
+            path = os.path.join(path, "index.html")
+            
+        elif os.path.isfile(path):
+            response.send_file(path)         
+            return event.stop()
+
+    response.not_found()
+    return event.stop()
+
