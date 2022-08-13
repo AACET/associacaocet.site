869853f2cd11ab632222a59b06b55bfcf889acb2
Jão <joao_bernardi@outlook.com>
2022-08-13 08:49:55 -0300

    `Bugs` - Prevent errors

--- routes/content.py
+++ routes/content.py
@@ -8,6 +8,10 @@ logger = logging.getLogger(__name__)
 
 @on("http_request", priority=100)
 def links(event: EventCall, host, path, request: Request, response: Response, server):
+    if not path:
+        response.internal_server_error()
+        event.stop()
+
     match os.path.basename(path):
         case "favicon.ico" as name:
             logging.info(f"Serving {name}")
