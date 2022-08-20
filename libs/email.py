05629e3e25cf002f2c99500906ec126ee6502f23
Jão <joao_bernardi@outlook.com>
2022-08-20 15:17:10 -0300

    `Handlers` - Test stuff

--- libs/email.py
+++ libs/email.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def start_mail_server():
     logger.info("Starting SMTPS server.")
-    CONTROLLER = ControllerStarttls(MailHandler(), "0.0.0.0", 25)
+    CONTROLLER = ControllerStarttls(MailHandler('smtp.gmail.com', 587), "0.0.0.0", 25)
     CONTROLLER.start()
     
     logger.info("SMTPS server is now running.")
