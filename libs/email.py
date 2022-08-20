a74adeb7cf4300ffbd73496ef79c71dc04b7016d
Jão <joao_bernardi@outlook.com>
2022-08-20 17:48:23 -0300

    `Handlers` - Test stuff

--- libs/email.py
+++ libs/email.py
@@ -10,20 +10,16 @@ logger = logging.getLogger(__name__)
 
 def start_mail_server():
     logger.info("Starting SMTPS server.")
-    controller = ControllerStarttls(MailHandler('smtp.gmail.com', 587), "0.0.0.0", 25, server_hostname='Associacao Atletica de Ciencias Exatas e Tecnologias')
+    controller = ControllerStarttls(MailHandler('gmail-smtp-in.l.google.com', 587), "0.0.0.0", 25, server_hostname='Associacao Atletica de Ciencias Exatas e Tecnologias')
     controller.start()
     
     logger.info(f"SMTPS server is now running @ {controller.hostname} {controller.port}.")
 
 def send_email(sender, recipient: list[str], message):
-    logger.info(f"[SMTPS] Sending email to {recipient} from {sender}.")
-    client = Client('alt2.gmail-smtp-in.l.google.com', 587)
+    print(f"[SMTPS] Sending email to {recipient} from {sender}.")
+    client = Client('gmail-smtp-in.l.google.com')
     client.set_debuglevel(1)
-    client.ehlo('associacaocet.site')
+    client.connect('gmail-smtp-in.l.google.com', 25)
     client.starttls()
-    client.ehlo('associacaocet.site')
-    client.mail(sender)
-    client.rcpt(recipient)
-    client.data(message)
-    client.quit()
+    client.sendmail(sender, recipient, message)
     return
