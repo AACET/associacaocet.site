fa87dcb5e32d32da0021a37ff17f871bca0de9a1
Jão <joao_bernardi@outlook.com>
2022-08-23 01:41:23 -0300

    `Links` - Some links redirects

--- routes/links.py
+++ routes/links.py
@@ -12,6 +12,13 @@ logger = logging.getLogger(__name__)
 
 @on("http_request", host="links.associacaocet.site")
 def links(event: EventCall, host, path, request, response: Response, server):
+    #fixed links
+    match os.path.basename(path):
+        case 'instagram':
+            response.define_code(301, "Permanently Moved")
+            response.headers["Location"] = "https://instagram.com/aacetupf/"
+            return event.stop()
+        
     fetch_files('links', path, response)
     return event.stop()
 
