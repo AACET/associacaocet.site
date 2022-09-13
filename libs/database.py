0255943d659d32f129ae1fadc4b2eff6c624c2f0
Jão <joao_bernardi@outlook.com>
2022-09-13 02:16:24 -0300

    `Database` - Add tables

--- libs/database.py
+++ libs/database.py
@@ -3,6 +3,63 @@ import sqlite3
 from types import FunctionType
 from typing import Any
 from . import config
+
+tables: dict[str, list[str]] = {
+    "users.sql": [
+        """
+        CREATE TABLE `Courses`(
+            `Name` TEXT,
+            `UUID` UUID,
+            PRIMARY KEY(UUID)  
+        )
+        """,
+        """
+        CREATE TABLE `Salts`(
+            `UUID` UUID,
+            `Hash` TEXT,
+            PRIMARY KEY(UUID)
+        )
+        """,
+        """
+        CREATE TABLE `Associated`(
+            `UUID` UUID,
+            `Username` TEXT,
+            `FirstName` TEXT,
+            `LastName` TEXT,
+            `Birthday` DATE,
+            `Email` TEXT,
+            `UPF_ID` TEXT,
+            `PhoneNumber` TEXT,
+            `CourseUUID` UUID,
+            `Password` TEXT,
+            `SaltUUID` UUID,
+            `Password_OTP` TOTP,
+            `Verification_OTP` TOTP,
+            `Active` BOOLEAN,
+            PRIMARY KEY(UUID),
+            FOREIGN KEY(SaltUUID)
+                REFERENCES Salts(UUID),
+            FOREIGN KEY(CourseUUID)
+                REFERENCES Courses(UUID)
+        )
+        """,
+    ],
+    "mail.sql": [
+    """
+    CREATE TABLE `Pool`(
+        `From` TEXT,
+        `To` TEXT,
+        `MessageID` TEXT,
+        `Subject` TEXT,
+        `Date` DATETIME,
+        `Data` LONGTEXT,      
+    )
+    """,
+    ],
+    "event_horizon.sql": []
+}
+
+
 class DatabaseWrapper:
     def __init__(self, function: FunctionType, databases: list[str]):
         self.function = function
